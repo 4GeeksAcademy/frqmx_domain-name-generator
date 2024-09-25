8121dde92153388a7b8f86c4095825b6af4ca3c4
Federico Requena Muxí <federico.requena@gmail.com>
2024-09-25 04:37:44 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+/* let multiplicationTable = [];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+for(let i=1; i<11; i++) {
+    for(let j=1; j<11; j++) {
+        multiplicationTable.push(`${i} * ${j} = ${i*j}`); 
+    }
+}  
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+console.log(multiplicationTable); */
+
+let prefix = ["big", "important", "very", "cool", "super"];
+let word = ["code", "coders", "tech", "stack", "geeks", "computing"];
+
+for (let i = 0; i < prefix.length; i++) {
+  for (let j = 0; j < word.length; j++) {
+    // console.log(`${prefix[i]}${word[j]}.com`);
+    let div = document.createElement("div");
+    div.innerText = `
+            ${prefix[i]}${word[j]}.com
+            ${prefix[i]}${word[j]}.net
+            ${prefix[i]}${word[j]}.io
+            ${prefix[i]}${word[j]}.us
+        `;
+
+    document.body.appendChild(div);
+  }
+}
